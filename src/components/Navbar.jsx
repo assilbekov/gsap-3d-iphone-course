ecabfa6ee339c33db34479b520c31316780a6e9a
assilbekov <askhat.assilbekov@gmail.com>
2024-04-01 04:04:49 -0300

    add navbar html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
-
+import { appleImg, bagImg, searchImg } from "../utils"
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center text-white p-4">
-      <h1>Logo</h1>
-      <ul className="flex space-x-4">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div>
+          {["Phones", "Macbooks", "Tablets"].map(nav => (
+            <div key={nav}>
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
   )
 }
