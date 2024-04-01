bbea76c0c2a0445fe2820b12d11196e5c21ccb71
assilbekov <askhat.assilbekov@gmail.com>
2024-04-01 04:00:26 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="bg-black">
+      <Navbar />
+      Hero
+      Highlights
+    </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center text-white p-4">
+      <h1>Logo</h1>
+      <ul className="flex space-x-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
