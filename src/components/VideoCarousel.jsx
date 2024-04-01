b429efb3b7a63fc8ed060991d6a4de926f068d9e
assilbekov <askhat.assilbekov@gmail.com>
2024-04-01 06:36:16 -0300

    init logic

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,6 +1,52 @@
+import { useEffect, useRef, useState } from "react"
 import { hightlightsSlides } from "../constants"
+import gsap from "gsap";
 
 export const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  })
+  const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
+  const { loadedData, setLoadedData } = useState([]);
+
+  useEffect(() => {
+    if (loadedData.length === hightlightsSlides.length) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();;
+      } else if (startPlay) {
+        videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData])
+
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if (span[videId]) {
+      let animation = gsap.to(span[videId], {
+        duration: 10,
+        width: "100%",
+        ease: "linear",
+        onUpdate: () => { },
+        onComplete: () => {
+          setVideo({
+            ...video,
+            isEnd: true,
+          })
+        }
+      })
+    }
+  }, [videoId, startPlay])
+
   return (
     <>
       <div className="flex items-center">
@@ -11,6 +57,12 @@ export const VideoCarousel = () => {
                 <video id="video" playsInline preload="auto" muted>
                   <source src={slide.video} type="video/mp4" />
                 </video>
+              </div>
+
+              <div className="absolute top-12 left-[5%] z-10">
+                {slide.textLists.map(text => (
+                  <p key={text} className="text-white md:text-2xl text-xl font-medium">{text}</p>
+                ))}
               </div>
             </div>
           </div>
