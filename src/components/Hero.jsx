461b77b403cb80c17caeb38c9fec48263a5767ee
assilbekov <askhat.assilbekov@gmail.com>
2024-04-01 04:30:31 -0300

    animate Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
 export const Hero = () => {
+  useGSAP(() => {
+    gsap.to("#hero", {
+      duration: 1,
+      opacity: 1,
+      delay: 0.5,
+    })
+  }, [])
+
   return (
-    <section className="bg-white text-black p-4">
-      <h1 className="text-4xl">Hero</h1>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">Iphone 15 Pro</p>
+      </div>
     </section>
   )
 }
