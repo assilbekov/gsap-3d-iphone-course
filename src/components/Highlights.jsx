04c25df1c52d83ab91bc3236ddc425b41bb8df6e
assilbekov <askhat.assilbekov@gmail.com>
2024-04-01 06:19:50 -0300

    add watchImg

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,30 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { watchImg } from "../utils"
+
 export const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", {
+      duration: 1,
+      opacity: 1,
+      y: 0,
+    })
+  }, [])
+
   return (
     <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
-        <div>
+        <div className="mb-12 w-full items-end justify-between">
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+          </div>
         </div>
       </div>
     </section>
