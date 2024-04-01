972c7e1c0e56fad353288d8c8ea3fb4b2f17d3ab
assilbekov <askhat.assilbekov@gmail.com>
2024-04-01 04:36:49 -0300

    add video to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
-import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
+import { useState } from "react"
+import { useGSAP } from "@gsap/react"
+import { heroVideo, smallHeroVideo } from "../utils"
 
 export const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
   useGSAP(() => {
     gsap.to("#hero", {
       duration: 1,
@@ -14,6 +17,12 @@ export const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">Iphone 15 Pro</p>
+
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   )
